2ab41217d366546a425c944973ac086aba71ae4a
Willy Mesa Medrano <jammdesigner@gmail.com>
2019-05-12 23:40:52 +0200

    Delete proyectos.js

--- JS/proyectos.js
+++
@@ -1,9 +0,0 @@
-function stackToTop(equipoid)	{
-	var equipo = document.getElementById(equipoid).style;
-	equipo.zIndex = "10";
-}
-
-function stackToBottom(equipoid)	{
-	var equipo = document.getElementById(equipoid).style;
-	equipo.zIndex = "0";
-}
