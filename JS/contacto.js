370ab9e61f4d05ee4f107b3bd5783d69f07c336e
Willy Mesa Medrano <jammdesigner@gmail.com>
2019-05-16 22:12:58 +0200

    Update contacto.js

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,33 +1,6 @@
-window.onload = function () {
-    displayResult();
-}
-function displayResult() {
-    xml = loadXMLDoc("https://github.com/JammDesigner/jammdesigner.github.io/blob/master/XML/contacto.xml");
-    xsl = loadXMLDoc("https://github.com/JammDesigner/jammdesigner.github.io/blob/master/XML/contacto.xslt");
-    // code for IE
-    if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
-        ex = xml.transformNode(xsl);
-        document.getElementById("contactoXML").innerHTML = ex;
-    }
-    // code for Chrome, Firefox, Opera, etc.
-    else if (document.implementation && document.implementation.createDocument) {
-        xsltProcessor = new XSLTProcessor();
-        xsltProcessor.importStylesheet(xsl);
-        resultDocument = xsltProcessor.transformToFragment(xml, document);
-        document.getElementById("contactoXML").appendChild(resultDocument);
-    }
-    console.log("result", resultDocument);
-}
+var boton=document.getElementById("enviar");
+boton.addEventListener("click",mensaje); 
 
-function loadXMLDoc(filename) {
-    if (window.ActiveXObject) {
-        xhttp = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    else {
-        xhttp = new XMLHttpRequest();
-    }
-    xhttp.open("GET", filename, false);
-    try { xhttp.responseType = "msxml-document" } catch (err) { } // Helping IE11
-    xhttp.send("");
-    return xhttp.responseXML;
+function mensaje(){
+    window.confirm("¡Gracias por contactar con nosotros! Le responderemos a la mayor brevedad posible");
 }
